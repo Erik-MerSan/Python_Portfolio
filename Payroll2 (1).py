4d5cffed8d1f1ed0eab12bdd1485d2072a89814b
Erik Mercado Sanchez <96037276+Erik-MerSan@users.noreply.github.com>
2023-07-11 22:49:54 -0500

    Update Payroll2 (1).py
    reformated code and added comments

--- Payroll2 (1).py
+++ Payroll2 (1).py
@@ -1,19 +1,20 @@
-# File: Payroll2.py
-# Student: Erik Mercado
-# UT EID: emm4376
 # Course Name: CS303E
 # Unique Number: 50695
-#
-# Date Created: September 18, 2020
-# Date Last Modified: September 18,2020
-# Description of Program: Figuring out an employee's pay slip with given information
+# Description of Program: a program to figure out an individual's paycheck based on the individual's pay information
+
+# first we create the input variables that the user will enter (employee name, # of hours worked, hourly wage, fed tax rate,
+# and state tax rate
 name1 = input("Enter employee name: ")
 workHours = float(input("Enter number of hours worked in a week: "))
 payRate = float(input("Enter hourly pay rate: "))
 fedTaxWithholdRate = float(input("Enter federal tax withholding rate: "))
 stateTaxWithholdRate = float(input("Enter state tax withholding rate: "))
+
+# total deduction and gross pay are created from input from the user
 totalDeduction = float((fedTaxWithholdRate*(payRate*workHours)) + (stateTaxWithholdRate*(payRate * workHours)))
 grossPay = float(payRate * workHours)
+
+# print out the output
 print()
 print("Employee Name: " + name1)
 print("Hours Worked: " + format(workHours, ".1f"))
